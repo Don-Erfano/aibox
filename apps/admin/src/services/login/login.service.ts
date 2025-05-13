5944e644c4dde2687ef7efabf9529b763ab0b860
Mahdi <mahdiprog81@gmail.com>
2025-05-13 13:56:21 +0330

    feat: admin login page

--- apps/admin/src/services/login/login.service.ts
+++ apps/admin/src/services/login/login.service.ts
@@ -1,11 +1,16 @@
 import { AxiosResponse } from 'axios';
-import { AbstractAPI } from '@aibox/services';
+
+import { AbstractAPI, INetworkResponse } from '@aibox/services';
+
+import { ILogiPayload, ILogiResponse } from './interface';
 
 export class AuthServices extends AbstractAPI {
   constructor() {
     super(``);
   }
-  public async postUserAuthLogin(data: any): Promise<AxiosResponse<any>> {
+  public async postUserAuthLogin(
+    data: ILogiPayload
+  ): Promise<AxiosResponse<INetworkResponse<ILogiResponse>>> {
     return await this.http.request({
       method: `POST`,
       url: `v1/user/register/login/`,
