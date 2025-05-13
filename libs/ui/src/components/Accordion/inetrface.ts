3f7ed16d20f213528378cc6001c866e7c981fb3c
MHJ-10 <mhjafari.dev@gmail.com>
2025-05-13 13:54:41 +0330

    fix: remove Accordion 1498

--- libs/ui/src/components/Accordion/inetrface.ts
+++
@@ -1,9 +0,0 @@
-type AccordionItem = {
-  title: React.ReactNode;
-  content: React.ReactNode;
-};
-
-export interface AccordionProps {
-  items: AccordionItem[];
-  type?: 'single' | 'multiple';
-}
