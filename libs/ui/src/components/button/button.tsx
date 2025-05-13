84c97f7980bf6eeec580591031de55152253d123
Mahdi <mahdiprog81@gmail.com>
2025-05-13 13:55:30 +0330

    feat: admin login page

--- libs/ui/src/components/button/button.tsx
+++ libs/ui/src/components/button/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '../../lib';
 
 const buttonVariants = cva(
-  `flex flex-nowrap cursor-pointer items-center justify-center
+  `flex flex-nowrap cursor-pointer w-full items-center justify-center
    gap-2 whitespace-nowrap rounded-md text-sm font-medium
    transition-all disabled:pointer-events-none disabled:opacity-50
    [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4
--- libs/ui/src/components/inputs/textfield/baseTextField.tsx
+++ libs/ui/src/components/inputs/textfield/baseTextField.tsx
@@ -1,49 +1,65 @@
 'use client';
 
-import { useRef } from 'react';
+import { forwardRef, useRef, useState } from 'react';
 import { IBaseTextfieldProps } from './interface';
 import { textfieldClassNames, textfieldWrapperClassNames } from './style';
+import { Eye, EyeOff } from 'lucide-react';
 
-const BaseTextField = ({
-  variant,
-  startAdornment,
-  endAdornment,
-  error,
-  direction,
-  ...props
-}: IBaseTextfieldProps) => {
-  const inputRef = useRef<HTMLInputElement>(null);
+const BaseTextField = forwardRef<HTMLInputElement, IBaseTextfieldProps>(
+  (
+    { variant, startAdornment, endAdornment, error, direction, ...props },
+    ref
+  ) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [type, setType] = useState(props.type);
 
-  const focusInput = () => {
-    inputRef.current?.focus();
-  };
+    const focusInput = () => {
+      inputRef.current?.focus();
+    };
 
-  return (
-    <div className="relative w-full">
-      <div
-        className={textfieldWrapperClassNames({ variant, error: !!error })}
-        onClick={focusInput}
-      >
-        {startAdornment}
-        <input
-          data-slot="input"
-          className={textfieldClassNames({ direction })}
-          ref={inputRef}
-          dir="auto"
-          {...props}
-        />
-        {endAdornment}
-      </div>
-      {error ? (
-        <p
-          className="absolute text-red-600 text-xs -bottom-4 left-auto right-2 w-fit"
-          lang="fa"
+    const handleChangeType = () => {
+      if (type === 'password') {
+        setType('text');
+      } else {
+        setType('password');
+      }
+    };
+
+    return (
+      <div className="relative w-full" ref={ref}>
+        <div
+          className={textfieldWrapperClassNames({ variant, error: !!error })}
+          onClick={focusInput}
         >
-          {error}
-        </p>
-      ) : null}
-    </div>
-  );
-};
+          {startAdornment}
+          <input
+            {...props}
+            data-slot="input"
+            className={textfieldClassNames({ direction })}
+            dir="auto"
+            ref={inputRef}
+            type={type}
+          />
+          {props.type === 'password' ? (
+            <div
+              className="cursor-pointer"
+              id="password-icon"
+              onClick={handleChangeType}
+            >
+              {type === 'password' ? <Eye /> : <EyeOff />}
+            </div>
+          ) : (
+            endAdornment
+          )}
+        </div>
+        {error ? (
+          <p className="absolute text-red-600 text-xs -bottom-4 left-auto right-2 w-fit">
+            {error}
+          </p>
+        ) : null}
+      </div>
+    );
+  }
+);
 
 export { BaseTextField };
