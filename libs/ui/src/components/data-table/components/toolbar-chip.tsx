789bc31d5eec27de006ff87b8688da2d7a29efbe
MHJ-10 <mhjafari.dev@gmail.com>
2025-05-14 16:07:06 +0330

    fix: chip button style 170

--- libs/ui/src/components/data-table/components/toolbar-chip.tsx
+++ libs/ui/src/components/data-table/components/toolbar-chip.tsx
@@ -9,8 +9,7 @@ export const ToolbarChip = ({ number, onIconClick }: ToolbarChipProps) => {
     <Badge className="bg-teal-600 rounded-[20px] pl-1 pr-3 py-px w-16 h-7 flex justify-center items-center gap-2 text-white">
       <span className="text-sm font-normal mt-1">{number}</span>
       <Button
-        variant="default"
-        className="!size-6 [&_svg]:!size-5 cursor-pointer hover:bg-teal-600 hover:!text-white"
+        className="!size-6 [&_svg]:!size-5 cursor-pointer bg-teal-600 text-white !border-teal-600"
         onClick={onIconClick}
       >
         <CloseIcon />
