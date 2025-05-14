b34158a111edbc0b8cb31f8e1752563f2b6fe796
MHJ-10 <mhjafari.dev@gmail.com>
2025-05-14 14:59:49 +0330

    fix: remove unused components 170

--- libs/ui/src/components/table-toolbar/constants/index.tsx
+++
@@ -1,8 +0,0 @@
-import { ChartLine as ChartIcon, Grid3X3 as TableIcon } from 'lucide-react';
-
-import { EViewModeButton, FilterIconButton } from '../types';
-
-export const viewModeButtons: FilterIconButton<EViewModeButton>[] = [
-  { name: EViewModeButton.CHART, icon: <ChartIcon />, hasChip: false },
-  { name: EViewModeButton.TABLE, icon: <TableIcon />, hasChip: false },
-];
--- libs/ui/src/components/table-toolbar/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './constants';
-export * from './components';
-export * from './TableToolbar';
-export * from './types';
