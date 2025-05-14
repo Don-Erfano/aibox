615344340f5a4d18d02ec3cffbb8206b882adb1d
MHJ-10 <mhjafari.dev@gmail.com>
2025-05-14 21:54:36 +0330

    fix: searchbar button styles 170

--- libs/ui/src/components/search-bar/search-bar.tsx
+++ libs/ui/src/components/search-bar/search-bar.tsx
@@ -1,14 +1,14 @@
 'use client';
 
+import clsx from 'clsx';
 import {
   ChevronLeft as ChevronLeftIcon,
   CircleX as ClearIcon,
   Search as SearchIcon,
 } from 'lucide-react';
 
-import { SearchBarProps } from './interface';
 import { Button } from '../button';
-import { Input } from '../input';
+import { SearchBarProps } from './interface';
 
 export const SearchBar = (props: SearchBarProps) => {
   const {
@@ -28,25 +28,25 @@ export const SearchBar = (props: SearchBarProps) => {
       {open ? (
         <div className="flex h-10 w-full items-center justify-between gap-[10px]">
           <div className="relative w-full">
-            <Input
-              className="flex h-10 w-full pl-8 rounded-[10px] !text-neutral-600 bg-white border border-teal-600 text-sm caret-teal-600 transition-colors duration-300 ease-in-out placeholder:text-neutral-500 hover:bg-neutral-100 hover:border-neutral-600 focus-visible:ring-0 focus:bg-white focus:!border-teal-600"
+            <input
+              className="flex outline-0 h-10 w-full pr-2 pl-8 rounded-[10px] !text-neutral-600 bg-white border border-teal-600 text-sm caret-teal-600 transition-colors duration-300 ease-in-out placeholder:text-neutral-500 hover:bg-neutral-100 hover:border-neutral-600 focus-visible:ring-0 focus:bg-white focus:!border-teal-600"
               placeholder={placeholder}
               value={value}
               onChange={(e) => onValueChange(e.target.value)}
             />
 
             <Button
-              size="icon"
-              variant="ghost"
-              className="!size-6 absolute !bg-white border-0 left-2 top-2 z-10 items-center justify-center p-0 disabled:cursor-not-allowed"
+              className={clsx(
+                'size-6 !bg-white !ring-0 [&_svg]:size-5 absolute border-0 left-2 top-2 z-10 items-center justify-center p-0 disabled:cursor-not-allowed',
+                {
+                  'text-neutral-800 hover:text-zinc-700': hasValue,
+                  'text-gray-300': !hasValue,
+                }
+              )}
               disabled={!hasValue}
               onClick={() => hasValue && onValueChange('')}
             >
-              {hasValue ? (
-                <ClearIcon className="text-neutral-800 hover:text-zinc-700" />
-              ) : (
-                <SearchIcon className="text-gray-200" />
-              )}
+              {hasValue ? <ClearIcon /> : <SearchIcon />}
             </Button>
           </div>
 
