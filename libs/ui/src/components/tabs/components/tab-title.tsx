1f53d1ddf14eaab9c21ed9305d23d902e1383a12
Negar_prv <negar.prv@gmail.com>
2025-05-15 14:09:31 +0330

    refactor: add clsx

--- libs/ui/src/components/tabs/components/tab-title.tsx
+++ libs/ui/src/components/tabs/components/tab-title.tsx
@@ -1,11 +1,14 @@
+import clsx from 'clsx';
 import { TabTitleProps } from '../types';
 
 export const TabTitle: React.FC<TabTitleProps> = ({ children, disabled }) => {
   return (
     <p
-      className={` group-focus:text-teal-600 group-data-[state=active]:text-teal-600 text-base font-medium inline-block whitespace-nowrap truncate text-ellipsis  ${
-        disabled ? 'text-gray-400' : 'hover:text-teal-600 text-zinc-600'
-      }`}
+      className={clsx(
+        'group-focus:text-teal-600 group-data-[state=active]:text-teal-600',
+        'text-base font-medium inline-block whitespace-nowrap truncate text-ellipsis',
+        disabled ? 'text-gray-400' : 'text-zinc-600 hover:text-teal-600'
+      )}
     >
       {children}
     </p>
--- libs/ui/src/components/tabs/components/tabs-trigger.tsx
+++ libs/ui/src/components/tabs/components/tabs-trigger.tsx
@@ -1,5 +1,6 @@
 import { Trigger } from '@radix-ui/react-tabs';
 import { TabsTriggerPropsWithoutClassName } from '../types';
+import clsx from 'clsx';
 
 export const TabsTrigger: React.FC<TabsTriggerPropsWithoutClassName> = ({
   disabled,
@@ -8,9 +9,11 @@ export const TabsTrigger: React.FC<TabsTriggerPropsWithoutClassName> = ({
   return (
     <Trigger
       data-slot="tabs-trigger"
-      className={`group px-6 pt-1 pb-3 h-full data-[state=active]:shadow-none border-b-[2.5px] border-transparent -mb-0.5 data-[state=active]:border-teal-600 transition-all duration-150 focus:outline-none ${
+      className={clsx(
+        'group px-6 pt-1 pb-3 h-full border-b-[2.5px] border-transparent -mb-0.5 transition-all duration-150 focus:outline-none',
+        'data-[state=active]:shadow-none data-[state=active]:border-teal-600',
         disabled ? 'cursor-default' : 'cursor-pointer'
-      }`}
+      )}
       {...props}
     />
   );
