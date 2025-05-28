7125f94ffc6c249afc0b1718ce54b44786737e50
MHJ-10 <mhjafari.dev@gmail.com>
2025-05-28 15:26:19 +0330

    fix: change toast position 1710

--- libs/ui/src/components/toast/toast.tsx
+++ libs/ui/src/components/toast/toast.tsx
@@ -27,6 +27,7 @@ const ToastContainer = () => (
       enter: 'animate-slide-in',
       exit: 'animate-slide-out',
     })}
+    style={{ top: 24, left: 36 }}
     toastClassName={() =>
       'rounded-sm overflow-hidden mb-3 shadow-[0_2px_12px_0_#0000003D] min-w-[320px] sm:min-w-[344px] max-w-[500px] w-fit min-h-12'
     }
