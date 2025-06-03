9957568881fd1327ff980b4dccc63407c9598541
Erfan Mirbabaei <erfan.mir@temp.com>
2025-06-03 13:20:47 +0000

    Erf feat uploader prod 1819

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -27,3 +27,5 @@ export * from './components/search-bar';
 export * from './components/form/autocomplete/custom-autocomplete';
 export * from './components/form/select';
 export * from './helper/time-formatter';
+export * from './components/form/uploader/aib-image-uploader';
+export * from './components/form/uploader/rhf-uploader';
