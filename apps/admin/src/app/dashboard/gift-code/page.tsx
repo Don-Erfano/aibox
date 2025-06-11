1c9594d2a907abfc3260d59a7ea6dc6588aef7b5
MHJ-10 <mhjafari.dev@gmail.com>
2025-06-11 18:37:49 +0330

    feat: gift code page 1824

---
+++ apps/admin/src/app/dashboard/gift-code/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+import { GiftCodeTable } from '@/components/pages/gift-code';
+
+const GiftCodePage: NextPage = () => <GiftCodeTable />;
+
+export default GiftCodePage;
