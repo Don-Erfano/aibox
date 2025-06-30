170dc0e5bf818b04da32ba708307311e036afbc2
Mahdi <mahdiprog81@gmail.com>
2025-06-30 23:12:39 +0330

    feat: gpus

--- apps/admin/src/app/dashboard/gpu/gpus/page.tsx
+++ apps/admin/src/app/dashboard/gpu/gpus/page.tsx
@@ -1,11 +1,8 @@
 'use client';
 
+import { GpusList } from '@/components';
 import { NextPage } from 'next';
-import { usePathname } from 'next/navigation';
 
-const Page: NextPage = () => {
-  const pathName = usePathname();
-  return <p>{pathName}</p>;
-};
+const Page: NextPage = () => <GpusList />;
 
 export default Page;
