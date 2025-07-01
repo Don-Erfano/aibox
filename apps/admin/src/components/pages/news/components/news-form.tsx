1722eafbccb9881d2983d4f2ac5b7af7e5f222f0
salemi <en.salemi75@gmail.com>
2025-07-01 15:14:48 +0330

    refactor: add form wrapper

--- apps/admin/src/components/pages/news/components/news-form.tsx
+++ apps/admin/src/components/pages/news/components/news-form.tsx
@@ -80,13 +80,17 @@ const NewsForm = ({
             />
           </FormWrapper>
 
-          <div className="max-w-[1376px] mx-4 sm:px-8 md:px-16 lg:px-6 xl:px-[60px]">
-            <RHFInput
-              name="content"
-              control={form.control}
-              label={strings.newsContent}
-              type="text"
-            />
+          <div className="flex flex-col w-full">
+            <div className="max-w-[1376px] xl:gap-x-[140px] 2xl:gap-x-[400px] xl:self-center grid grid-cols-1 gap-10 lg:place-content-between lg:grid-cols-[minmax(0,_480px)_minmax(0,_480px)] px-4 sm:px-8 md:px-16 lg:px-6 xl:px-[60px]">
+              <div className="lg:col-span-2">
+                <RHFInput
+                  name="content"
+                  control={form.control}
+                  label={strings.newsContent}
+                  type="text"
+                />
+              </div>
+            </div>
           </div>
 
           <div className="flex gap-5 justify-center mt-12">
