38380ce3bc88f657347629919b2817ee7354112e
MHJ-10 <mhjafari.dev@gmail.com>
2025-07-10 09:52:57 +0330

    fix: remove duplicated remote pattern 1984

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -28,11 +28,6 @@ const nextConfig = {
         pathname: '/media/pictures/**',
         search: '',
       },
-      {
-        protocol: 'https',
-        hostname: 'dev.fileshare.nextaibox.com',
-        pathname: '/media/pictures/**',
-      },
     ],
   },
 };
